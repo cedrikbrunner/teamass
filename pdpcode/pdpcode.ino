d673bdac8db3a7e095d614badbd3991bd9870e91
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-03-25 17:44:33 +0100

    Add files via upload

--- pdpcode/pdpcode.ino
+++ pdpcode/pdpcode.ino
@@ -1,4 +1,5 @@
 #include <CheapStepper.h>
+int sensor=0;
 
 // next, declare the stepper
 // and connect pins 8,9,10,11 to IN1,IN2,IN3,IN4 on ULN2003 board
@@ -16,7 +17,7 @@ void setup() {
 
   // let's run the stepper at 12rpm (if using 5V power) - the default is ~16 rpm
 
-  stepper.setRpm(20);
+  stepper.setRpm(12);
 
   // let's print out the RPM to make sure the setting worked
   
@@ -27,13 +28,12 @@ void setup() {
   // now let's set up our first move...
   // let's move a half rotation from the start point
 
-  stepper.newMoveTo(moveClockwise, 2048);
+  
   /* this is the same as: 
    * stepper.newMoveToDegree(clockwise, 180);
    * because there are 4096 (default) steps in a full rotation
    */
- 
-  
+ stepper.newMove(moveClockwise,1);
 }
 
 void loop() {
@@ -41,28 +41,24 @@ void loop() {
   // we need to call run() during loop() 
   // in order to keep the stepper moving
   // if we are using non-blocking moves
-  
+ 
   stepper.run();
-
-  ////////////////////////////////
-  // now the stepper is moving, //
-  // let's do some other stuff! //
-  ////////////////////////////////
-
+ 
+  
   // let's check how many steps are left in the current move:
-  
   int stepsLeft = stepper.getStepsLeft();
-
   // if the current move is done...
-  
   if (stepsLeft == 0){
-
+  Serial.println("Da bini");
     // let's print the position of the stepper to serial
     Serial.print("stepper position: "); Serial.print(stepper.getStep());
     Serial.println();
     // let's start a new move 
-    stepper.newMoveDegrees (moveClockwise, 180); // move 180 degrees from current position
+    stepper.newMove (moveClockwise, 100); // move 180 degrees from current position
+    if (sensor = 1){
+        Serial.println("bini ready!");
+    }
+   
 
   }
-
 }
