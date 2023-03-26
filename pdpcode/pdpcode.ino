f2a8f8feeaacc9cb1dead9caafc8472bcd08120a
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-03-26 15:23:50 +0200

    Add files via upload

--- pdpcode/pdpcode.ino
+++ pdpcode/pdpcode.ino
@@ -1,6 +1,9 @@
 #include <CheapStepper.h>
 long steps = 0; //long for 32 bits
 int n=100; // number of steps each intervall
+int sensor = 2; //sensor at pin X
+int s1 = 0;
+int savesteps = 0;
 
 CheapStepper stepper (8,9,10,11); 
 boolean moveClockwise = true; // false for CCW
@@ -10,15 +13,30 @@ boolean moveClockwise = true; // false for CCW
 void setup() {  
   Serial.begin(9600);
   Serial.println("Ready to start moving!");
-  stepper.setRpm(17);
+  stepper.setRpm(17); //works fine untill 17, after that not enough torque to move
+  pinMode(sensor, INPUT);
 }
 
 //------------------
 
 void loop() {
-  steppermove();
-  print();
+  readsensor();
+  steps = 0;
+  Serial.println(steps);
   
+  while (s1 == HIGH){
+    steppermove();
+    readsensor();
+    if (steps <= 2000) {
+      print();
+    }
+    if (steps >= 6000) {
+      print();
+    }
+  }
+  stepperstop();
+  savesteps += steps;
+  Serial.print("saved steps:"); Serial.println(savesteps);
 }
 
 //------------------
@@ -30,7 +48,17 @@ void steppermove () {
     steps += 1;
     }
 }
+void stepperstop () {
 
+  for (int s=0; s<0; s++){
+    stepper.step(moveClockwise);
+    }
+}
 void print() {
   Serial.println(steps);
+}
+
+void readsensor() {
+  s1 = digitalRead (sensor);
+  Serial.print("Sensor: "); Serial.println(s1);
 }
