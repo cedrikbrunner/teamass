75083bfb3a9e1c987b08a7c5463dca945a86b904
cedrikbrunner <113671718+cedrikbrunner@users.noreply.github.com>
2023-03-26 15:30:00 +0200

    Add files via upload

--- pdpcode/pdpcode.ino
+++ pdpcode/pdpcode.ino
@@ -3,9 +3,9 @@ long steps = 0; //long for 32 bits
 int n=100; // number of steps each intervall
 int sensor = 2; //sensor at pin X
 int s1 = 0;
-int savesteps = 0;
+long savesteps = 0;
 
-CheapStepper stepper (8,9,10,11); 
+CheapStepper stepper (8,9,10,11);  //pins for driver IN1, IN2, IN3, IN4
 boolean moveClockwise = true; // false for CCW
 
 //--------------------
@@ -14,28 +14,28 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Ready to start moving!");
   stepper.setRpm(17); //works fine untill 17, after that not enough torque to move
-  pinMode(sensor, INPUT);
+  pinMode(sensor, INPUT); //define the sensor pin as an INPUT
 }
 
 //------------------
 
 void loop() {
   readsensor();
-  steps = 0;
+  steps = 0; //befor it enters the while loop always set steps back to 0
   Serial.println(steps);
   
-  while (s1 == HIGH){
-    steppermove();
-    readsensor();
-    if (steps <= 2000) {
+  while (s1 == HIGH){ //while the sensor is high the stepper moves
+    steppermove(); // n step at the time
+    readsensor(); //then read sensor again
+    if (steps <= 2000) { //just for checking if it works
       print();
     }
     if (steps >= 6000) {
       print();
     }
   }
-  stepperstop();
-  savesteps += steps;
+  stepperstop(); //sensor LOW stepper stops
+  savesteps += steps; // and write the steps which it drove into savesteps
   Serial.print("saved steps:"); Serial.println(savesteps);
 }
 
@@ -43,22 +43,22 @@ void loop() {
 
 void steppermove () {
 
-  for (int s=0; s<n; s++){
+  for (int s=0; s<n; s++){ //n number of steps in the loop, we have to check for accuracy 
     stepper.step(moveClockwise);
     steps += 1;
     }
 }
 void stepperstop () {
 
-  for (int s=0; s<0; s++){
+  for (int s=0; s<0; s++){ // 0 steps = stopp :D
     stepper.step(moveClockwise);
     }
 }
 void print() {
-  Serial.println(steps);
+  Serial.println(steps); 
 }
 
-void readsensor() {
-  s1 = digitalRead (sensor);
+void readsensor() { //reading sensor value and give it to s1 (HIGH or LOW)
+  s1 = digitalRead (sensor); 
   Serial.print("Sensor: "); Serial.println(s1);
 }
