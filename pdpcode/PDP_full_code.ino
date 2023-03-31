f5c791707250d1bd8067079c42199ea55054bebc
Bennett Pearce <97134488+bennettinak@users.noreply.github.com>
2023-03-31 09:44:14 +0200

    Add files via upload

--- pdpcode/PDP_full_code.ino
+++ pdpcode/PDP_full_code.ino
@@ -2,19 +2,27 @@
 #include <CheapStepper.h> //not sure if we still plan to use this library
 
 //-----Pins-----
+//put variables that control Arduino pins here
+int sensor1_pin = 2;
+int sensor2_pin = 3;
+CheapStepper stepper (8,9,10,11);
 
 //-----Variables-----
+//put other variables here
+long stepper_steps_total = 0;
+long board_length_steps = 0;
 
 //-----Setup-----
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600) //this sets up the Serial Monitor for use later in the code
 }
 
 //-----Main Loop-----
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  read_sensor(1); //calls function ans passes it value of one, this 
 }
 
-//-----
+//-----Functions------
+int read_sensor(int sensor_num) { //
+  Serial.print(sensor_num);
+}
