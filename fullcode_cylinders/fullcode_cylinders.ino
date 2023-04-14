208d401aeabdeccac3475139ae485d4211243276
Bennett Pearce <97134488+bennettinak@users.noreply.github.com>
2023-04-14 11:52:38 +0200

    Add files via upload

--- fullcode_cylinders/fullcode_cylinders.ino
+++ fullcode_cylinders/fullcode_cylinders.ino
@@ -1,30 +1,40 @@
 // ---------- Pins ----------
+const int stepPin = 2;        // Yellow driver cable
+const int dirPin = 3;         // Blue driver cable
 const int relayC1 = 4;        // Pin for relay pneumatic cylinder C1
 const int relayC2 = 5;        // Pin for relay pneumatic cylinder C2
 const int relayC3 = 6;        // Pin for relay pneumatic cylinder C3
+const int S1 = 7;             // Pin for IR Sensor 1
+const int S2 = 8;             // Pin for IR Sensor 2
 const int S3 = 9;             // Pin for pneumatic cylinder C1 sensor 3
 const int S4 = 10;            // Pin for pneumatic cylinder C1 sensor 4
 const int S5 = 11;            // Pin for pneumatic cylinder C2 sensor 5
 const int S6 = 10;            // Pin for pneumatic cylinder C2 sensor 6
 const int S7 = 11;            // Pin for pneumatic cylinder C3 sensor 7
 const int S8 = 12;            // Pin for pneumatic cylinder C3 sensor 8
+const int relayfan = 13;      // Pin for relay to start/stop the fan
 
 
 // ---------- Variables ----------
+bool board_labeled = false
 
 
 // ----------Setup ---------- 
 void setup() {
+  pinMode(S1, INPUT); // IR sensor 1
+  pinMode(S2, INPUT); // IR sensor 2
   pinMode(S3, INPUT); // C1 sensor at retracted position
   pinMode(S4, INPUT); // C1 sensor at expanded position
   pinMode(S5, INPUT); // C2 sensor at retracted position
   pinMode(S6, INPUT); // C2 sensor at expanded position
   pinMode(S7, INPUT); // C3 sensor at retracted position
   pinMode(S8, INPUT); // C3 sensor at expanded position
+  pinMode(stepPin,OUTPUT);
+  pinMode(dirPin,OUTPUT);
   pinMode(relayC1,OUTPUT);
   pinMode(relayC2,OUTPUT);
   pinMode(relayC3,OUTPUT);
-
+  pinMode(relayfan,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -34,8 +44,11 @@ void loop() {
     extend_C1();
     retract_C1();
   }
-}
 
+  if (digitalRead(S1) == LOW) {
+    board_labeled = false; //for testing purposes to reset the system
+  }
+}
 
 //----------Functions ----------
 void extend_C1() {
